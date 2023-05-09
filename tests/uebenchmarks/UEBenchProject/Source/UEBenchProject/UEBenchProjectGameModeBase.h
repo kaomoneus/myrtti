9351358216ad87fb733b3ba0ee84252408cf7768
Stepan Dyatkovskiy (kaomoneus) <ml@dyatkovskiy.com>
2023-05-09 11:12:11 +0400

    Added UEBenchProject (empty)

---
+++ tests/uebenchmarks/UEBenchProject/Source/UEBenchProject/UEBenchProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEBenchProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEBENCHPROJECT_API AUEBenchProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
